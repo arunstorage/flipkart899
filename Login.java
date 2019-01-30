8a6a968aa89b1378b99370d9fda2c61760b3c4fe
P Nageswara Rao <nageswara.rao@gamutgurus.com>
2019-01-30 10:19:27 +0530

    some pending change

--- Login.java
+++ Login.java
@@ -2,11 +2,11 @@ public class Login
 {
 	public static void main(String args[])
 	{
-		int a = 15;
+		int a = 25;
 		for(i=0;i<=40;i++)
 		{
 			System.out.println("Hello World!");
-			System.out.println("Hello World!");
+			System.out.println("Hello Class!");
 			System.out.println("Hello World!");
 			System.out.println("Hello World!");
 		}
