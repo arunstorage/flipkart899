c4c0041d7649d3a4ab6ba3fd12dd3aa022740370
P Nageswara Rao <nageswara.rao@nexamatic.com>
2019-03-20 10:35:27 +0530

    remove dups

--- Login.java
+++ Login.java
@@ -2,12 +2,10 @@ public class Login
 {
 	public static void main(String args[])
 	{
-		int a = 25;
+		int a = 10
+
 		for(i=0;i<=40;i++)
 		{
-			System.out.println("Hello World!");
-			System.out.println("Hello Class!");
-			System.out.println("Hello World!");
 			System.out.println("Hello World!");
 		}
 	}
